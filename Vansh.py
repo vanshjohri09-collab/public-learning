345a6b63ead5c40d3df18425f8291def6dc56cfe
Vansh johri <vanshjohri09@gmail.com>
2025-08-29 18:02:01 +0530

    Update Vansh.py
    Hi this is stone paper game

--- Vansh.py
+++ Vansh.py
@@ -1,1 +1,15 @@
+Print(f"welcome to the game ")
+Yours=int(input("choose one form stone paper scissor ")
+Import random 
+Choices=["stone","paper","scissor "]
+Computer=random.choose(choices)
+If(yours=stone and computer=paper):
+ Print(f"you chooses {Yours} and computer choses {Computer} so computer wins this round )
+elif(yours=paper and computer=scissor):
+ Print(f"you chooses {Yours} and computer choses {Computer} so computer wins this round )
+elif(yours=scissor and computer=stone):
+ Print(f"you chooses {Yours} and computer choses {Computer} so computer wins this round )
+else:
+Print(f"you chooses {Yours} and computer choses {Computer} so you wins this round )
+
 
